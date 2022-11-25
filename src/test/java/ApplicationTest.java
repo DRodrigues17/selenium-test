4446c6514f9694d1c5affa9c5a5f0c3790dadda1
drodrigues17 <adanielluiz71@gmail.com>
2022-11-25 20:50:13 -0300

    versão final

--- src/test/java/ApplicationTest.java
+++
@@ -1,9 +0,0 @@
-public class ApplicationTest {
-
-    AcessarPaginaTest acessarPaginaTest;
-    LocalizarLojasTest localizarLojasTest;
-    LocalizarLojaAleatoriaTest localizarLojaAleatoriaTest;
-
-    //TODO
-    // ajeitar classe para que testes rodem de forma consecutiva
-}
